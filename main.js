027da78d9dd01ff16e2fa545605102e8156efbc5
ojhaugen15 <ojhaugen15@gmail.com>
2025-09-11 21:30:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('testing')
+console.log('TESTING')
 current_depth = 0
 
 function transpileIsp (programText, currentIndex, currentOutput) {
@@ -53,6 +53,8 @@ function transpileBlock (programText, currentIndex, currentOutput) {
    outputText = concatenateStrings(outputText, '\n')
    currentIndex = addNumbers(searchIndex, 1)
    console.log('currentIndex: ', currentIndex)
+   console.log('searchIndex: ', searchIndex)
+   console.log('outputText: ', outputText)
    transpiledBlock = transpileBlock(programText, currentIndex, outputText)
    return transpiledBlock
   }
